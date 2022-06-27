80c2b6c26ad5015f7e1281eb5e486a289448a376
Andrew Elgert <andrew.elgert@gmail.com>
2022-06-27 13:45:13 -0400

    Updated install script to handle zsh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ln -s ~/.dotfiles/bashrc ~/.bashrc
+ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/profile ~/.profile
 ln -s ~/.dotfiles/bash_profile ~/.bash_profile
